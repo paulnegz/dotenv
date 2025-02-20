3013691dd885bf90d7afdf23cbcbce556babea05
Paul Negedu <paul.negedu@yahoo.com>
2025-02-19 19:24:59 -0600

    terminal addtions

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,17 +1,45 @@
-return {
-  require('codeium').setup {
-    vim.api.nvim_create_autocmd('TermOpen', {
-      callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-      end,
-    }),
-    vim.keymap.set('n', '<C-`>', function()
-      vim.cmd.vnew()
-      vim.cmd.term()
-      vim.cmd.wincmd 'J'
-      vim.api.nvim_win_set_height(0, 7)
-      vim.cmd.startinsert()
-    end),
-  },
-}
+vim.api.nvim_create_autocmd('TermOpen', {
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+-- Function to find an existing terminal buffer
+local function find_existing_terminal()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+      -- Optionally, check for specific terminal buffers by name or other criteria
+      return buf
+    end
+  end
+  return nil
+end
+
+-- Function to toggle the terminal
+function ToggleTerminal()
+  local term_buf = find_existing_terminal()
+
+  if term_buf and vim.api.nvim_buf_is_loaded(term_buf) then
+    -- If terminal buffer exists and is loaded, toggle its visibility
+    local win = vim.fn.bufwinnr(term_buf)
+    if win ~= -1 then
+      -- If the terminal is open in a window, close that window
+      vim.api.nvim_win_close(vim.fn.win_getid(win), true)
+    else
+      -- If the terminal exists but is not visible, open it in a split
+      vim.api.nvim_command('split | buffer ' .. term_buf)
+    end
+  else
+    -- If no terminal buffer exists, create a new terminal in a split
+    vim.api.nvim_command 'split | terminal'
+  end
+end
+
+-- Optionally, set a keybinding to toggle the terminal
+vim.api.nvim_set_keymap('n', '<C-`>', ':lua ToggleTerminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-`>', ':lua ToggleTerminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-`>', '<cmd>stopinsert<CR> :lua ToggleTerminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-`>', '<cmd>stopinsert<CR> :lua ToggleTerminal()<CR>', { noremap = true, silent = true })
+
+return {}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
@@ -14,8 +14,12 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    close_if_last_window = true, -- Close Neo-tree if it's the last window open
+    popup_border_style = 'rounded', -- Nice rounded borders
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
-      follow_current_file = true,
+      -- follow_current_file = true,
       filtered_items = {
         visible = true,
         show_hidden_count = true,
@@ -23,14 +27,33 @@ return {
         hide_gitignored = false,
         hide_hidden = false,
       },
+      preview = {
+        enabled = true,
+      },
     },
     window = {
       mappings = {
         ['\\'] = 'close_window',
-        ['/'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+        [']'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+        ['l'] = 'focus_preview',
+        ['<C-b>'] = { 'scroll_preview', config = { direction = 10 } },
+        ['<C-f>'] = { 'scroll_preview', config = { direction = -10 } },
       },
     },
+
+    event_handlers = {
+      {
+        event = 'neo_tree_window_after_open',
+        handler = function()
+          --local state = require('neo-tree.sources.filesystem')._state
+        end,
+      },
+    },
+
     default_component_configs = {
+      preview = {
+        enabled = true,
+      },
       icon = {
         folder_closed = '📁',
         folder_open = '📂',
